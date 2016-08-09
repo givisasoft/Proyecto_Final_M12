96d5847599923fc78f77ecbef16ef871ff2b2bb4
Giancarlo <giancarlovillafana@gmail.com>
2016-08-09 17:21:43 -0500

    este es un commit

---
+++ app/src/test/java/facebook/givisa/com/proyecto_final_m12/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package facebook.givisa.com.proyecto_final_m12;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
